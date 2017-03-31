2ef9a2ca2d60ac359bea0989ae8de36b71fa3507
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-31 18:25:17 +0200

    #4: Convert the Permutations object into a PHP Generator.

--- src/Generators/Permutations.php
+++ src/Generators/Permutations.php
@@ -2,16 +2,36 @@
 
 namespace drupol\phpermutations\Generators;
 
-use drupol\phpermutations\Permutations as PermutationsClass;
+use drupol\phpermutations\Combinatorics;
 
 /**
  * Class Permutations.
  *
  * @package drupol\phpermutations\Generators
  *
- * @author Mark Wilson <mark@89allport.co.uk>
+ * Inspired by the work of Mark Wilson <mark@89allport.co.uk>
  */
-class Permutations extends PermutationsClass {
+class Permutations extends Combinatorics {
+
+  /**
+   * The combinations generator.
+   *
+   * @var \drupol\phpermutations\Generators\Combinations
+   */
+  public $combinations;
+
+  /**
+   * Combinatorics constructor.
+   *
+   * @param array $dataset
+   *   The dataset.
+   * @param int|null $length
+   *   The length.
+   */
+  public function __construct(array $dataset = array(), $length = NULL) {
+    parent::__construct($dataset, $length);
+    $this->combinations = new Combinations($dataset, $this->getLength());
+  }
 
   /**
    * Alias of the get() method.
@@ -20,11 +40,31 @@ class Permutations extends PermutationsClass {
    *   The prime generator.
    */
   public function generator() {
-    return $this->get($this->getDataset());
+    return $this->get($this->getDataset(), $this->getLength());
   }
 
   /**
-   * The generator.
+   * The combination generator.
+   *
+   * @param array $dataset
+   *   The dataset.
+   * @param int $length
+   *   The length.
+   *
+   * @codingStandardsIgnoreStart
+   * @return \Generator
+   * @codingStandardsIgnoreEnd
+   */
+  protected function get(array $dataset, $length) {
+    foreach ($this->combinations->generator() as $combination) {
+      foreach ($this->getPermutations($combination) as $current) {
+        yield $current;
+      }
+    }
+  }
+
+  /**
+   * The permutations generator.
    *
    * @param array $dataset
    *   The dataset.
@@ -33,7 +73,7 @@ class Permutations extends PermutationsClass {
    * @return \Generator
    * @codingStandardsIgnoreEnd
    */
-  protected function get(array $dataset) {
+  protected function getPermutations(array $dataset) {
     foreach ($dataset as $key => $firstItem) {
       $remaining = $dataset;
       array_splice($remaining, $key, 1);
@@ -41,11 +81,34 @@ class Permutations extends PermutationsClass {
         yield [$firstItem];
         continue;
       }
-      foreach ($this->get($remaining) as $permutation) {
+      foreach ($this->getPermutations($remaining) as $permutation) {
         array_unshift($permutation, $firstItem);
         yield $permutation;
       }
     }
+  }
+
+  /**
+   * Convert the generator into an array.
+   *
+   * @return array
+   *   The elements.
+   */
+  public function toArray() {
+    $data = array();
+
+    foreach ($this->generator() as $value) {
+      $data[] = $value;
+    }
+
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function count() {
+    return $this->combinations->count() * $this->fact($this->getLength());
   }
 
 }
